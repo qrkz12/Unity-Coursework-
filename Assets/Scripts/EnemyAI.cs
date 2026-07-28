82d77f3e5103849175ad1d069cb284020024106f
Michael Quirke <mquirke98@hotmail.co.uk>
2026-07-28 17:11:12 +0100

    Updated enemy AI, auto chase player when spawned, created enemy prefab

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,17 +3,28 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform player;
+    private Transform player;
     private NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("No GameObject with the Player tag was found.");
+        }
     }
 
     void Update()
     {
-        if (player != null)
+        if (player != null && agent.isOnNavMesh)
         {
             agent.SetDestination(player.position);
         }
