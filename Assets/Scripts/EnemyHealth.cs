31c5df08ea3f01330f7496baf89602679e1f14d1
Michael Quirke <mquirke98@hotmail.co.uk>
2026-07-28 23:54:46 +0100

    Added enemy healthbars

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log(gameObject.name + " health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
