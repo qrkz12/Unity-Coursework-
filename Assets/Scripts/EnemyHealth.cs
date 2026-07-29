888989af82b820d5cad56fe1b3d33dc128f8625e
Michael Quirke <mquirke98@hotmail.co.uk>
2026-07-29 02:14:25 +0100

    Started projectile for player 'shooting'

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-
     private int currentHealth;
+
+    public Slider healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
-        Debug.Log(gameObject.name + " health: " + currentHealth);
+        healthBar.value = currentHealth;
+
+        Debug.Log($"{gameObject.name} took {damage} damage. Health remaining: {currentHealth}");
 
         if (currentHealth <= 0)
         {
